288a6cd739db92ace1bf7aa61d698fd19caea0d4
ziqiang <ziqiang.wang@davyin.com>
2019-04-29 17:34:57 +0800

    bug fix

--- Provision/Config/Drupal/Services.php
+++ Provision/Config/Drupal/Services.php
@@ -30,7 +30,7 @@ class Provision_Config_Drupal_Services extends Provision_Config {
         ]
       ]
     ];
-    drush_command_invoke_all('provision_drupal_services', d()->uri, $data);
+    $data = drush_command_invoke_all('provision_drupal_services', d()->uri, $data);
     
     $this->data['content'] = Symfony\Component\Yaml\Yaml::dump($data,4);
     if(empty($data)){
